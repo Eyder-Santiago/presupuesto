3191f9aa22920524140c25904cad1a50f4c0022b
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2023-02-23 00:31:48 -0500

    modelo egreso creado -2

---
+++ src/app/modelo/egreso.ts
@@ -0,0 +1,10 @@
+export class Egreso{
+    constructor(
+        public id:number=0,
+        public descripcion:string="",
+        public total:number=0
+    ){
+        
+
+    }
+}
--- src/app/services/egreso.service.ts
+++ src/app/services/egreso.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Egreso } from '../modelo/egreso';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EgresoService {
 
-  constructor() { }
+  //Arreglo donde se guardarán los egresos
+  public egresos:Array<Egreso>=[];
+
+
+  //Para crear los servicios de egreso
+
+  constructor(
+    private http : HttpClient
+  ) { }
+
+  public agregarEgreso(egreso:Egreso){
+    //agregamos el egreso que viene al arreglo de egresos
+    this.egresos.push(egreso);
+  }
+
+  
 }
