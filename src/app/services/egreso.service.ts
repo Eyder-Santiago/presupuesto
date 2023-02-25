fecead307becf72c8a87377e769a5ac1086710bb
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2023-02-24 19:48:35 -0500

    servicio egreso listo -4

--- src/app/services/egreso.service.ts
+++ src/app/services/egreso.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Egreso } from '../modelo/egreso';
+import { environment } from 'src/environments/environment'; //se importó enviroment
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,42 @@ export class EgresoService {
     this.egresos.push(egreso);
   }
 
+  //esto viene de environment.ts
+  UrlBase:string=environment.UrlBackend;
+
+  public getEgresos(){
+    //defino la url donde está el servicio
+    let url = this.UrlBase + '/egresos';
+    let header=new HttpHeaders();
+    header.append('Content-Type','aplication/json')
+    header.append('Access-Control-Allow-Origin','http://localhost');
+    return this.http.get<Egreso[]>(url,{headers:header});
+  }
+
+  public crearEgreso(egreso:Egreso){
+    let url = this.UrlBase + '/egresos';
+    let header=new HttpHeaders();
+    header.append('Content-Type','aplication/json')
+    header.append('Access-Control-Allow-Methods','"POST, GET,DELETE,PUT"')
+    header.append('Access-Control-Allow-Origin','http://localhost');
+    return this.http.post(url,JSON.stringify(egreso),{headers:header});
+  }
+
+  public editarEgreso(egreso:Egreso){
+    let url = this.UrlBase + '/egresos';
+    let header=new HttpHeaders();
+    header.append('Content-Type','aplication/json')
+    header.append('Access-Control-Allow-Methods','"POST, GET,DELETE,PUT"')
+    header.append('Access-Control-Allow-Origin','http://localhost');
+    return this.http.put(url,JSON.stringify(egreso),{headers:header});
+  }
+
+  public eliminarEgreso(egreso:Egreso){
+    let  url = this.UrlBase + '/egresos/'+ egreso.id;
+    let header=new HttpHeaders();
+    header.append('Content-Type','aplication/json')
+    header.append('Access-Control-Allow-Origin','http://localhost');
+    return this.http.delete(url,{headers:header});
+  }
   
 }
